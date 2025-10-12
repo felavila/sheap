8e1e3a09e53589ec00ac1598a06a86f2b29b6305
felavila <felipeavila41@gmail.com>
2025-10-12 11:51:21 -0300

    balmer repair

--- sheap/Sheapectral/Utils/SpectralSetup.py
+++ sheap/Sheapectral/Utils/SpectralSetup.py
@@ -265,7 +265,7 @@ def ensure_sfd_data(sfd_path: Path = None):
         "SFD_mask_4096_sgp.fits",
     ]
 
-    base_url = "https://github.com/kbarbary/sfddata/raw/master"
+    base_url = "https://github.com/kbarbary/sfddata"
 
     missing = [fname for fname in files if not (sfd_path / fname).exists()]
     if not missing:
